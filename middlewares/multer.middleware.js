d9e38ec879e43d3a04e15abeb76effdd29831c96
Prajal190904 <pranjalvyas45@gmail.com>
2024-02-17 23:29:02 +0530

    document format and size limited added in upload document api

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,6 +1,21 @@
 const multer = require("multer");
 
 const storage = multer.memoryStorage({});
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, limits: {
+    fileSize: 200*1024
+}}).single('document');
 
-module.exports = upload;
+const uploadDocs=(req,res,next)=>{
+    upload(req,res, function(err) {
+        if(err){
+            return res.status(400).json({message:"File size limit exceeded."})
+        }
+        if(req.file.mimetype!="image/jpeg" && req.file.mimetype!="image/png" && req.file.mimetype!="application/pdf")
+        {
+            return res.status(400).json({message:"File type not allowed"})
+        }
+        next();
+    })
+}
+
+module.exports = uploadDocs;
